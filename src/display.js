0a49994588fb4bae96e03557147bb523871cfcb6
Aleks <24192100+aleksishere@users.noreply.github.com>
2023-07-11 16:20:00 +0200

    Finished website!

--- src/display.js
+++ src/display.js
@@ -2,15 +2,10 @@ function makeButtonsClicable() {
     let btns = Array.from(document.querySelectorAll('li'));
     btns[0].addEventListener('click', displayHome);
     btns[1].addEventListener('click', displayMenu);
+    btns[2].addEventListener('click', displayContact);
 }
 
-function displayHome() {
-    const textArr = [
-        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis itaque nostrum atque temporibus. Autem perspiciatis tenetur quaerat, deleniti libero, voluptates fuga accusantium fugiat expedita, explicabo commodi ex eligendi non eaque.',
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus rerum eum maxime, distinctio illo alias reprehenderit voluptates saepe doloribus similique laudantium illum corrupti laborum possimus est quasi nisi minus sapiente.',
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus repellat molestiae corporis dicta laudantium veritatis veniam similique. Quasi consequatur, non, fugiat beatae sed quo repellendus sequi magnam ea maxime quod!'
-    ];
-
+function buildTopBar() {
     const content = document.getElementById('content');
     content.innerHTML = '';
 
@@ -54,6 +49,28 @@ function displayHome() {
     let contactLi = document.createElement('li')
     contactLi.innerText = 'Contact';
     list.appendChild(contactLi);
+}
+
+function buildFooter() {
+    let footer = document.createElement('footer');
+    content.appendChild(footer);
+    let footerP = document.createElement('p');
+    footerP.classList.add('no-margin');
+    footerP.innerText = 'made by aleksishere';
+    footer.appendChild(footerP);
+
+    makeButtonsClicable();
+}
+
+function displayHome() {
+
+    buildTopBar();
+
+    const textArr = [
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis itaque nostrum atque temporibus. Autem perspiciatis tenetur quaerat, deleniti libero, voluptates fuga accusantium fugiat expedita, explicabo commodi ex eligendi non eaque.',
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus rerum eum maxime, distinctio illo alias reprehenderit voluptates saepe doloribus similique laudantium illum corrupti laborum possimus est quasi nisi minus sapiente.',
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus repellat molestiae corporis dicta laudantium veritatis veniam similique. Quasi consequatur, non, fugiat beatae sed quo repellendus sequi magnam ea maxime quod!'
+    ];
 
     let article = document.createElement('article');
     content.appendChild(article);
@@ -72,15 +89,7 @@ function displayHome() {
         tabImg.classList.add('tab-img');
         tab.appendChild(tabImg);
     }
-
-    let footer = document.createElement('footer');
-    content.appendChild(footer);
-    let footerP = document.createElement('p');
-    footerP.classList.add('no-margin');
-    footerP.innerText = 'made by aleksishere';
-    footer.appendChild(footerP);
-
-    makeButtonsClicable();
+    buildFooter();
 }
 
 function displayMenu() {
@@ -99,49 +108,7 @@ function displayMenu() {
         ],
     ];
 
-    const content = document.getElementById('content');
-    content.innerHTML = '';
-
-    let topWrapper = document.createElement('div');
-    topWrapper.id = 'top-wrapper';
-    content.appendChild(topWrapper);
-
-    let header = document.createElement('header');
-    header.classList.add('section-background');
-    topWrapper.appendChild(header);
-
-    let title = document.createElement('h1');
-    title.classList.add('no-margin');
-    title.innerText = "Hylian's Restaurant";
-    header.appendChild(title);
-
-    let subtext = document.createElement('h5');
-    subtext.classList.add('no-margin');
-    subtext.innerText = "Just throw it into the pot!";
-    header.appendChild(subtext);
-
-    let nav = document.createElement('nav');
-    nav.classList.add('section-background');
-    topWrapper.appendChild(nav);
-
-    let borderLine = document.createElement('div');
-    borderLine.classList.add('border-line');
-    nav.appendChild(borderLine);
-
-    let list = document.createElement('ul');
-    borderLine.appendChild(list);
-
-    let homeLi = document.createElement('li')
-    homeLi.innerText = 'Home';
-    list.appendChild(homeLi);
-
-    let menuLi = document.createElement('li')
-    menuLi.innerText = 'Menu';
-    list.appendChild(menuLi);
-
-    let contactLi = document.createElement('li')
-    contactLi.innerText = 'Contact';
-    list.appendChild(contactLi);
+    buildTopBar();
 
     let article = document.createElement('article');
     content.appendChild(article);
@@ -172,15 +139,52 @@ function displayMenu() {
         imgWrapper.appendChild(imgMenu);
     }
 
-    let footer = document.createElement('footer');
-    content.appendChild(footer);
-    let footerP = document.createElement('p');
-    footerP.classList.add('no-margin');
-    footerP.innerText = 'made by aleksishere';
-    footer.appendChild(footerP);
-
-
-    makeButtonsClicable();
+    buildFooter();
 }
 
-export {displayHome,displayMenu};
+function displayContact() {
+    buildTopBar();
+
+    const contact = [
+        ['Hey!', 'Thanks!'],
+        ['I just want to thank zeldawiki.wiki, zeldadungeon.net, Hylia Serif team and The Odin Project community! And quick reminder that all characters, images are property of Nintendo.',
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore rerum possimus maxime repellat molestiae cumque nam eligendi cum harum ea tempore, magnam quae sit amet totam, corporis aut odit obcaecati.',
+        ],
+        ['https://cdn.wikimg.net/en/zeldawiki/images/thumb/6/6f/ST_Princess_Zelda_Artwork.png/443px-ST_Princess_Zelda_Artwork.png',
+         'https://static.wikia.nocookie.net/zelda_gamepedia_en/images/d/de/ST_Link_Artwork.png',
+        ],
+    ]
+
+    let article = document.createElement('article');
+    content.appendChild(article);
+
+    for (let index = 0; index < contact[0].length; index++) {
+        let contactItem = document.createElement('div');
+        contactItem.classList.add('contact-item');
+        article.appendChild(contactItem);
+    
+        let textContact = document.createElement('div');   
+        contactItem.appendChild(textContact);
+
+        let textContactHeader = document.createElement('p');
+        textContactHeader.classList.add('no-margin');
+        textContactHeader.innerText = contact[0][index];
+        textContact.appendChild(textContactHeader);
+
+        let textContactText = document.createElement('p');
+        textContactText.classList.add('no-margin');
+        textContactText.innerText = contact[1][index];
+        textContact.appendChild(textContactText);
+
+        let imgWrapper = document.createElement('div');
+        contactItem.appendChild(imgWrapper);
+
+        let img = document.createElement('img');
+        img.setAttribute('src', contact[2][index]);
+        imgWrapper.appendChild(img);
+    }
+
+    buildFooter();
+}
+
+export {displayHome,displayMenu, displayContact};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './style.css';
-import { displayHome,displayMenu } from './displayHome';
+import {displayHome} from './display';
 
-let btns = Array.from(document.querySelectorAll('li'));
-btns[0].addEventListener('click', displayHome);
-btns[1].addEventListener('click', displayMenu);
+displayHome();
